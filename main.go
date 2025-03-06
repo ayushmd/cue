f126630e0204f8b821cb601b8d1a075291c84e67
Ayush Deshmukh <ayushmdcr@gmail.com>
2025-03-07 01:39:35 +0530

    removed main

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"container/heap"
-	"fmt"
 	"sync"
 	"time"
 )
@@ -111,38 +110,38 @@ func (q *TTLQueue) Pop() any {
 	return el.(*element).data
 }
 
-type TTLItem struct {
-	id        int
-	createdAt int64
-}
+// type TTLItem struct {
+// 	id        int
+// 	createdAt int64
+// }
 
-func main() {
-	ttlq := NewTTLQueue()
+// func main() {
+// 	ttlq := NewTTLQueue()
 
-	// Background queue listner
-	go func() {
-		for {
-			select {
-			case job := <-ttlq.Subscribe():
-				jobj := job.(*TTLItem)
-				fmt.Printf("Recieved Job %d: Created At: %d Recieved At: %d\n", jobj.id, jobj.createdAt, time.Now().Unix())
-			}
-		}
-	}()
+// 	// Background queue listner
+// 	go func() {
+// 		for {
+// 			select {
+// 			case job := <-ttlq.Subscribe():
+// 				jobj := job.(*TTLItem)
+// 				fmt.Printf("Recieved Job %d: Created At: %d Recieved At: %d\n", jobj.id, jobj.createdAt, time.Now().Unix())
+// 			}
+// 		}
+// 	}()
 
-	ttlq.Push(&TTLItem{
-		id:        1,
-		createdAt: time.Now().Unix(),
-	}, time.Now().Add(10*time.Second).Unix())
+// 	ttlq.Push(&TTLItem{
+// 		id:        1,
+// 		createdAt: time.Now().Unix(),
+// 	}, time.Now().Add(10*time.Second).Unix())
 
-	ttlq.Push(&TTLItem{
-		id:        2,
-		createdAt: time.Now().Unix(),
-	}, time.Now().Add(5*time.Second).Unix())
+// 	ttlq.Push(&TTLItem{
+// 		id:        2,
+// 		createdAt: time.Now().Unix(),
+// 	}, time.Now().Add(5*time.Second).Unix())
 
-	ttlq.Push(&TTLItem{
-		id:        3,
-		createdAt: time.Now().Unix(),
-	}, time.Now().Add(2*time.Second).Unix())
-	select {}
-}
+// 	ttlq.Push(&TTLItem{
+// 		id:        3,
+// 		createdAt: time.Now().Unix(),
+// 	}, time.Now().Add(2*time.Second).Unix())
+// 	select {}
+// }
